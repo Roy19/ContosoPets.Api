30b0ab92ff8800e1a7e67f48d9a8d882345ad836
Aritra Sur Roy <aritra1997@hotmail.com>
2020-10-31 14:07:19 +0530

    Fix build issues

--- build-pipeline.sh
+++
@@ -1,9 +0,0 @@
-chmod +x /home/runner/work/ContosoPets.Api/ContosoPets.Api/build.sh
-RELEASE_BRANCH=master
-echo "Branch: $BRANCH_NAME"
-if [ $BRANCH_NAME = $RELEASE_BRANCH ]
-    then
-        /home/runner/work/ContosoPets.Api/ContosoPets.Api/build.sh --target=CI --Branch-Name=master --API-Key=${{env.NUGET_GPR_KEY}}
-    else
-        /home/runner/work/ContosoPets.Api/ContosoPets.Api/build.sh --target=CI --Branch-Name=master --Build-Number=${GITHUB_RUN_NUMBER} --API-Key=${{env.NUGET_GPR_KEY}}
-fi
